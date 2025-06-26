c5a1e54a6dc3504fe93f20bfddb0d79d0b9b07e5
Xebeth <github@north-edge.com>
2025-06-26 20:49:50 +0100

    install evdi

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,22 +3,6 @@
 set -ouex pipefail
 
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+wget https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v6.1.1-1/fedora-42-displaylink-1.14.10-1.github_evdi.x86_64.rpm
+dnf5 install -y fedora-42-displaylink-1.14.10-1.github_evdi.x86_64.rpm
+rm -f fedora-42-displaylink-1.14.10-1.github_evdi.x86_64.rpm
